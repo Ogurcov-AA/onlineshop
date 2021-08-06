b60b917324ce335275545a48a63f2b24800b0400
Sasha Ogurtsov <sanekLLIypa@yandex.ru>
2021-08-06 19:18:25 +0300

    chore: init vuetify

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <v-toolbar>
+  <searchElement/>
+  </v-toolbar>
+</div>
+</template>
+
+<script>
+import searchElement from "../components/searchElement";
+
+export default {
+  components: {
+    searchElement
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
