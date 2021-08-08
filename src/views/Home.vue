51b721f39da56b12373a6d7cacb908d71571a8ac
Sasha Ogurtsov <sanekLLIypa@yandex.ru>
2021-08-08 17:48:09 +0300

    feat: add filtering components and cards for the start page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,32 @@
 <template>
-<div>
-</div>
+  <section class="main-field">
+    <mainPageFilter v-bind:message="'test message'"/>
+    <div class="cards-panel">
+      <card
+          v-for="item in 10" :key="item"
+          v-bind:title="'Фильтр внутренний PICK UP 60Фильтр внутренний'"
+          v-bind:description="'YINGTAI T11 мобильный телефон для пожилых,Лучшая функция,телефон для пожилых,2,2 дюймов,fm фонарь,скорость,DialSOS,большая кнопка,динамик,клавиатура с русским шрифтом,GSM'"
+          v-bind:price="62.5"
+      />
+    </div>
+  </section>
 </template>
 
 <script>
-
+import card from "../components/card";
+import mainPageFilter from "../components/mainPageFilter";
 export default {
-  components:{
+  components: {
+    card,
+    mainPageFilter
   },
 }
 </script>
 
 <style scoped>
-
+.cards-panel{
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
 </style>
