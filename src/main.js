e90f725ac3a56b78d5d8015cf0ef7314fc895452
Sasha Ogurtsov <sanekLLIypa@yandex.ru>
2021-08-28 23:45:03 +0300

    refactor: init auth user and load basket before create application

--- src/main.js
+++ src/main.js
@@ -2,21 +2,35 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import DefaultLayout from './layouts/DefaultLayout.vue'
-import {router} from "./router/router";
+import BasketLayout from "./layouts/SearchAndAuthLayout"
+import ErrorLayout from "./layouts/ErrorLayout"
+import AdminLayouts from './layouts/AdminLayout'
+import LoadingLayout from "./layouts/LoadingLayout"
+import {router} from "./router/router"
 import './plugins/firebase'
-import auth from "./auth";
+import auth from "./auth"
 import store from './store/index'
 
+export const eventBus = new Vue()
+
 Vue.component('default-layout', DefaultLayout)
+Vue.component('basket-layout', BasketLayout)
+Vue.component('error-layout', ErrorLayout)
+Vue.component('admin-layout', AdminLayouts)
+Vue.component('loading-layout',LoadingLayout)
 
 Vue.config.productionTip = false
+auth.init(this, async (user)=>{
+   await store.dispatch('setCurrentUser',user)
+    if(user!==null){
+        await store.dispatch('getBasket',user.uid)
+    }
 
 new Vue({
-  beforeCreate () {
-    auth.init(this)
-  },
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+    store,
+    vuetify,
+    auth,
+    router,
+    render: h => h(App)
 }).$mount('#app')
+})
